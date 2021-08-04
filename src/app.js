d53f263b41b1b32260eca923eec71861eefb43f3
Eber Cedeño <cedenoparedes04@gmail.com>
2021-08-04 09:33:13 -0400

    adding docker to the project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import config from "./config";
 import productRoutes from "./routes/producnt.routes";
-
+import morgan from "morgan";
 const app = express();
 
 /// settings
 app.set("port", config.port);
+app.use(morgan("dev"));
 
 //middlewares
 // const bodyParser = require('body-parser');
